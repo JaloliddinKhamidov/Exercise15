c2e85a9169bd583fd25b59e65fb0db78cb5dfce0
Jaloliddin <93047359+JaloliddinKhamidov@users.noreply.github.com>
2023-05-23 18:26:22 +0200

    Add files via upload

---
+++ Exercise15 Task1/first-project-java-spring/src/main/java/pl/edu/vistula/firstprojectjavaspring/controller/HelloController.java
@@ -0,0 +1,20 @@
+package pl.edu.vistula.firstprojectjavaspring.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+//@RestController
+@Controller
+public class HelloController {
+@GetMapping(value = "/")
+    public String hello(){
+    return "Hello world! This is my first spring controller!";}
+
+    @GetMapping("/greeting")
+    public String greeting (@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model){
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+}
